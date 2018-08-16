b86593a3f218b151eaa01dc0de133153c5f2c00b
Geoff Sobering <geoff@geoffs.net>
2018-08-16 11:59:39 -0500

    Made InoExample1 a single-file sketch and InoExample2 the sketch with separate Task1.h and Task1.cpp files.

--- examples/InoExample1/InoExample1.ino
+++ examples/InoExample1/InoExample1.ino
@@ -1,4 +1,25 @@
-#include "Task1.h"
+#include <GSSArduTasks.h>
+
+class Task1 : public Task
+{
+  public:
+    void setup() override
+    {
+      pinMode(LED_BUILTIN , OUTPUT);
+    }
+    long loop(long currTime_ms) override
+    {
+      digitalWrite(LED_BUILTIN, nextState);
+
+      if (nextState != HIGH) nextState = HIGH;
+      else                   nextState = LOW;
+
+      return currTime_ms + 200;
+    }
+
+  private:
+    uint8_t nextState = HIGH;
+};
 
 Task1 task1;
 
